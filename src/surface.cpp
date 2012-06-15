fdc16c11b10884794b3cfdb373556fec20f4836a
Sol Boucher <slb1566@rit.edu>
2012-06-14 20:13:58 -0400

    Added y-dimensional passthrough cropping to eliminate any objects falling outside the robot's height range

--- src/surface.cpp
+++ src/surface.cpp
@@ -1,20 +1,27 @@
 #include "ros/ros.h"
 #include "pcl_ros/point_cloud.h"
 #include "pcl/point_types.h"
+#include "pcl/filters/passthrough.h"
 #include "pcl/filters/voxel_grid.h"
 
 ros::Publisher outgoing;
 
 void callback(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& in)
 {
+	pcl::PassThrough<pcl::PointXYZ> crop;
 	pcl::VoxelGrid<pcl::PointXYZ> filter;
-	pcl::PointCloud<pcl::PointXYZ> out;
+	pcl::PointCloud<pcl::PointXYZ>::Ptr out(new pcl::PointCloud<pcl::PointXYZ>());
 
-	filter.setInputCloud(in);
+	crop.setInputCloud(in);
+	crop.setFilterFieldName("y");
+	crop.setFilterLimits(0, 0.37);
+	crop.filter(*out);
+
+	filter.setInputCloud(out);
 	filter.setLeafSize(0.04f, 0.04f, 0.04f);
-	filter.filter(out);
+	filter.filter(*out);
 
-	outgoing.publish(out);
+	outgoing.publish(*out);
 }
 
 int main(int argc, char** argv)
