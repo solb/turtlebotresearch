a575d7b782b44ce73c2d233dcb74a92b0415060a
Sol Boucher <slb1566@rit.edu>
2012-06-15 18:33:30 -0400

    Moved all constants onto the ROS parameter server
    Allowing one to manipulate them in real time during the program's run.

--- src/surface.cpp
+++ src/surface.cpp
@@ -4,21 +4,33 @@
 #include "pcl/filters/passthrough.h"
 #include "pcl/filters/voxel_grid.h"
 
+ros::NodeHandle* node;
 ros::Publisher outgoing;
 
 void callback(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& in)
 {
+	std::string PASSTHROUGH_DIMENSION;
+	double PASSTHROUGH_MIN, PASSTHROUGH_MAX, VOXELGRID_LEAFSIZE;
+
+	//read updated values for constants
+	node->getParam("/xbot_surface/passthrough_dimension", PASSTHROUGH_DIMENSION);
+	node->getParam("/xbot_surface/passthrough_min", PASSTHROUGH_MIN);
+	node->getParam("/xbot_surface/passthrough_max", PASSTHROUGH_MAX);
+	node->getParam("/xbot_surface/voxelgrid_leafsize", VOXELGRID_LEAFSIZE);
+
 	pcl::PassThrough<pcl::PointXYZ> crop;
 	pcl::VoxelGrid<pcl::PointXYZ> filter;
 	pcl::PointCloud<pcl::PointXYZ>::Ptr out(new pcl::PointCloud<pcl::PointXYZ>());
 
+	//crop the cloud
 	crop.setInputCloud(in);
-	crop.setFilterFieldName("y");
-	crop.setFilterLimits(0, 0.37);
+	crop.setFilterFieldName(PASSTHROUGH_DIMENSION);
+	crop.setFilterLimits(PASSTHROUGH_MIN, PASSTHROUGH_MAX);
 	crop.filter(*out);
 
+	//downsample cloud
 	filter.setInputCloud(out);
-	filter.setLeafSize(0.04f, 0.04f, 0.04f);
+	filter.setLeafSize((float)VOXELGRID_LEAFSIZE, (float)VOXELGRID_LEAFSIZE, (float)VOXELGRID_LEAFSIZE);
 	filter.filter(*out);
 
 	outgoing.publish(*out);
@@ -27,9 +39,24 @@ void callback(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& in)
 int main(int argc, char** argv)
 {
 	ros::init(argc, argv, "surface");
-	ros::NodeHandle node;
+	node=new ros::NodeHandle();
 
-	ros::Subscriber incoming=node.subscribe("/cloud_throttled", 1, callback);
-	outgoing=node.advertise< pcl::PointCloud<pcl::PointXYZ> >("/cloud_surfaces", 1);
+	//declare constants
+	node->setParam("/xbot_surface/passthrough_dimension", "y");
+	node->setParam("/xbot_surface/passthrough_min", 0.0);
+	node->setParam("/xbot_surface/passthrough_max", 0.36);
+	node->setParam("/xbot_surface/voxelgrid_leafsize", 0.03);
+
+	//request and pass messages
+	ros::Subscriber incoming=node->subscribe("/cloud_throttled", 1, callback);
+	outgoing=node->advertise< pcl::PointCloud<pcl::PointXYZ> >("/cloud_surfaces", 1);
 	ros::spin();
+
+	//clean up constants
+	node->deleteParam("/xbot_surface/passthrough_dimension");
+	node->deleteParam("/xbot_surface/passthrough_min");
+	node->deleteParam("/xbot_surface/passthrough_max");
+	node->deleteParam("/xbot_surface/voxelgrid_leafsize");
+
+	delete node;
 }
