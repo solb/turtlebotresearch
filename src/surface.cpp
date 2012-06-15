923809435225315c57ce7d1abe7b92b9730c18e3
Sol Boucher <slb1566@rit.edu>
2012-06-15 16:11:56 -0400

    Implemented plane segmentation using RANSAC.
    This may or may not acutally be useful; perhaps the likelihood of the former would be greater if we were to refrain from cropping out the floor...?

--- src/surface.cpp
+++ src/surface.cpp
@@ -3,25 +3,45 @@
 #include "pcl/point_types.h"
 #include "pcl/filters/passthrough.h"
 #include "pcl/filters/voxel_grid.h"
+#include "pcl/segmentation/sac_segmentation.h"
+#include "pcl/sample_consensus/method_types.h"
 
 ros::Publisher outgoing;
 
 void callback(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& in)
 {
+	pcl::PointCloud<pcl::PointXYZ>::Ptr working(new pcl::PointCloud<pcl::PointXYZ>());
+	pcl::PointCloud<pcl::PointXYZ> out;
 	pcl::PassThrough<pcl::PointXYZ> crop;
-	pcl::VoxelGrid<pcl::PointXYZ> filter;
-	pcl::PointCloud<pcl::PointXYZ>::Ptr out(new pcl::PointCloud<pcl::PointXYZ>());
+	pcl::VoxelGrid<pcl::PointXYZ> downsample;
+	pcl::SACSegmentation<pcl::PointXYZ> planes;
+	pcl::PointIndices inliers;
+	pcl::ModelCoefficients coefficients;
 
 	crop.setInputCloud(in);
 	crop.setFilterFieldName("y");
 	crop.setFilterLimits(0, 0.37);
-	crop.filter(*out);
+	crop.filter(*working);
 
-	filter.setInputCloud(out);
-	filter.setLeafSize(0.04f, 0.04f, 0.04f);
-	filter.filter(*out);
+	downsample.setInputCloud(working);
+	downsample.setLeafSize(0.04f, 0.04f, 0.04f);
+	downsample.filter(*working);
 
-	outgoing.publish(*out);
+	planes.setInputCloud(working);
+	planes.setMethodType(pcl::SAC_RANSAC);
+	planes.setModelType(pcl::SACMODEL_PLANE);
+	planes.setDistanceThreshold(0.01);
+	planes.setMaxIterations(1000);
+	planes.segment(inliers, coefficients);
+
+	for(std::vector<int>::iterator point=inliers.indices.begin(); point<inliers.indices.end(); point++)
+		out.push_back((*working)[*point]);
+		//ROS_INFO("%d", *point); //wtf?!
+		//ROS_INFO("(%f,%f,%f)", (*working)[*point].x, (*working)[*point].y, (*working)[*point].z); //wth...
+	//ROS_INFO("inliers: %d vs. points: %d so SAME: %d", (int)inliers.indices.size(), (int)out.points.size(), inliers.indices.size()==out.points.size());
+
+	working->swap(out);
+	outgoing.publish(*working);
 }
 
 int main(int argc, char** argv)
