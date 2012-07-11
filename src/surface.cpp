3cef8799893286e7daa2b9862583f87502bf066a
Sol Boucher <slb1566@rit.edu>
2012-07-11 17:23:30 -0400

    Added radius-driven outlier removal to filter false positives.
    Also optimized constants in order to attain false positive--free data.
    Other additions:
    - X-coordinate bumper regions on the sides to eliminate noise due to jagged edges

--- src/surface.cpp
+++ src/surface.cpp
@@ -5,6 +5,7 @@
 #include "pcl/features/integral_image_normal.h"
 #include "pcl/kdtree/organized_data.h"
 #include "pcl/features/boundary.h"
+#include "pcl/filters/radius_outlier_removal.h"
 #include "geometry_msgs/Twist.h"
 
 ros::NodeHandle* node;
@@ -22,14 +23,16 @@ void callback(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& in)
 	Jd2/\ui, f)d$
 	^ExA;j
 	#endif
-	double CROP_XRADIUS, CROP_YMIN, CROP_YMAX, CROP_ZMIN, CROP_ZMAX, FLOOR_CLOSEY, FLOOR_CLOSEZ, FLOOR_FARY, FLOOR_FARZ, FLOOR_TOLERANCE, EDGES_SEARCHRADIUS, DRIVE_LINEARSPEED, DRIVE_ANGULARSPEED;
-	bool FLOOR_TRANSFORM, DRIVE_MOVE, DISPLAY_DECISIONS;
+	double CROP_XRADIUS, CROP_XBUMPER, CROP_YMIN, CROP_YMAX, CROP_ZMIN, CROP_ZMAX, FLOOR_CLOSEY, FLOOR_CLOSEZ, FLOOR_FARY, FLOOR_FARZ, FLOOR_TOLERANCE, EDGES_SEARCHRADIUS, OUTLIERS_SEARCHRADIUS, DRIVE_LINEARSPEED, DRIVE_ANGULARSPEED;
+	int OUTLIERS_MINNEIGHBORS;
+	bool FLOOR_TRANSFORM, OUTLIERS_REMOVE, DRIVE_MOVE, DISPLAY_DECISIONS;
 
 	//read updated values for "constants" ... may be generated from declarations using the macro:
 	#if 0
 	^fsrg2f"F/l"vyt"f,wdt)"vPvb~f;d$a;j
 	#endif
 	node->getParam("/xbot_surface/crop_xradius", CROP_XRADIUS);
+	node->getParam("/xbot_surface/crop_xbumper", CROP_XBUMPER);
 	node->getParam("/xbot_surface/crop_ymin", CROP_YMIN);
 	node->getParam("/xbot_surface/crop_ymax", CROP_YMAX);
 	node->getParam("/xbot_surface/crop_zmin", CROP_ZMIN);
@@ -40,9 +43,12 @@ void callback(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& in)
 	node->getParam("/xbot_surface/floor_farz", FLOOR_FARZ);
 	node->getParam("/xbot_surface/floor_tolerance", FLOOR_TOLERANCE);
 	node->getParam("/xbot_surface/edges_searchradius", EDGES_SEARCHRADIUS);
+	node->getParam("/xbot_surface/outliers_searchradius", OUTLIERS_SEARCHRADIUS);
 	node->getParam("/xbot_surface/drive_linearspeed", DRIVE_LINEARSPEED);
 	node->getParam("/xbot_surface/drive_angularspeed", DRIVE_ANGULARSPEED);
+	node->getParam("/xbot_surface/outliers_minneighbors", OUTLIERS_MINNEIGHBORS);
 	node->getParam("/xbot_surface/floor_transform", FLOOR_TRANSFORM);
+	node->getParam("/xbot_surface/outliers_remove", OUTLIERS_REMOVE);
 	node->getParam("/xbot_surface/drive_move", DRIVE_MOVE);
 	node->getParam("/xbot_surface/display_decisions", DISPLAY_DECISIONS);
 
@@ -62,16 +68,17 @@ void callback(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& in)
 	pcl::IntegralImageNormalEstimation<pcl::PointXYZ, pcl::Normal> normalize;
 	pcl::OrganizedDataIndex<pcl::PointXYZ>::Ptr index(new pcl::OrganizedDataIndex<pcl::PointXYZ>);
 	pcl::BoundaryEstimation<pcl::PointXYZ, pcl::Normal, pcl::Boundary> edgeDetect;
+	pcl::RadiusOutlierRemoval<pcl::PointXYZ> remove;
 	pcl::PointCloud<pcl::PointXYZ>::Ptr points(new pcl::PointCloud<pcl::PointXYZ>);
 	pcl::PointCloud<pcl::Normal>::Ptr normals(new pcl::PointCloud<pcl::Normal>);
 	pcl::PointCloud<pcl::Boundary> boundaries;
-	pcl::PointCloud<pcl::PointXYZ> boundaryPoints;
-	geometry_msgs::Twist directions;
+	pcl::PointCloud<pcl::PointXYZ>::Ptr boundaryPoints(new pcl::PointCloud<pcl::PointXYZ>);
+	geometry_msgs::Twist directions; //TODO re-integrate
 
 	//crop to focus exclusively on the approximate range of floor points
 	crop.setInputCloud(in);
 	crop.setFilterFieldName("x");
-	crop.setFilterLimits(-CROP_XRADIUS, CROP_XRADIUS);
+	crop.setFilterLimits(-CROP_XRADIUS-CROP_XBUMPER, CROP_XRADIUS+CROP_XBUMPER);
 	crop.setKeepOrganized(true);
 	crop.filter(*points);
 
@@ -103,12 +110,15 @@ void callback(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& in)
 		}
 	}
 
+	//optimize constants TODO remove
+	//double TEMP=10.0;
+	//bool TEMP=false;
+	//if(node->hasParam("/xbot_test")) node->getParam("/xbot_test", TEMP);
+
 	//compute surface normals
 	normalize.setInputCloud(points);
-	normalize.setNormalEstimationMethod(normalize.AVERAGE_DEPTH_CHANGE);
-	//normalize.setNormalEstimationMethod(normalize.AVERAGE_3D_GRADIENT); TODO recheck
-	//normalize.setMaxDepthChangeFactor(0.02f); TODO recheck
-	//normalize.setNormalSmoothingSize(10.0f); TODO recheck
+	//normalize.setNormalSmoothingSize((float)TEMP); //TODO low numbers may improve leading edges detection
+	//normalize.setDepthDependentSmoothing(TEMP); //TODO may decrease size of false positive clusters
 	normalize.compute(*normals);
 
 	//detect edge points
@@ -119,13 +129,22 @@ void callback(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& in)
 	edgeDetect.compute(boundaries);
 
 	//prepare the detected points for display
-	boundaryPoints.header=points->header;
+	boundaryPoints->header=points->header;
 	for(pcl::PointCloud<pcl::Boundary>::iterator it=boundaries.begin(); it<boundaries.end(); it++)
-		if(it->boundary_point) boundaryPoints.push_back((*points)[(int)(it-boundaries.begin())]);
+		if(it->boundary_point && (*points)[(int)(it-boundaries.begin())].x!=std::numeric_limits<float>::quiet_NaN() && fabs((*points)[(int)(it-boundaries.begin())].x)<=CROP_XRADIUS) //this is a boundary point and doesn't fall within either of the lateral bumper regions
+			boundaryPoints->push_back((*points)[(int)(it-boundaries.begin())]);
+
+	if(OUTLIERS_REMOVE && boundaryPoints->size()>0)
+	{
+		remove.setInputCloud(boundaryPoints);
+		remove.setRadiusSearch(OUTLIERS_SEARCHRADIUS);
+		remove.setMinNeighborsInRadius(OUTLIERS_MINNEIGHBORS);
+		remove.filter(*boundaryPoints);
+	}
 
 	//display the clouds we've created
 	visualizer.publish(*points);
-	visualizer2.publish(boundaryPoints);
+	visualizer2.publish(*boundaryPoints);
 }
 
 int main(int argc, char** argv)
@@ -133,8 +152,11 @@ int main(int argc, char** argv)
 	ros::init(argc, argv, "surface");
 	node=new ros::NodeHandle;
 
+	node->deleteParam("/xbot_test"); //for quickly optimizing constants TODO remove
+
 	//declare constants
 	node->setParam("/xbot_surface/crop_xradius", 0.25); //the robot's radius
+	node->setParam("/xbot_surface/crop_xbumper", 0.1); //this region will be added to xradius to obtain the true cropping range, but any edgepoints falling within it will be ignored to guard against jagged edges
 	node->setParam("/xbot_surface/crop_ymin", 0.3); //should be lt [floor_closey,floor_fary]
 	node->setParam("/xbot_surface/crop_ymax", 1.0); //should be gt [floor_closey,floor_fary]
 	node->setParam("/xbot_surface/crop_zmin", 0.0); //should be lt [floor_closez,floor_farz]
@@ -144,10 +166,13 @@ int main(int argc, char** argv)
 	node->setParam("/xbot_surface/floor_fary", 0.47); //y-coordinate of floor's far boundary, used to approximate its slope TODO recalculate
 	node->setParam("/xbot_surface/floor_farz", 2.5); //z-coordinate of floor's far boundary, used to approximate its slope TODO recalculate
 	node->setParam("/xbot_surface/floor_tolerance", 0.02); //maximum allowable y-coordinate devation of floor points
-	node->setParam("/xbot_surface/edges_searchradius", 0.03);
+	node->setParam("/xbot_surface/edges_searchradius", 0.04); //higher number means smaller false positive patches, but slower processing speed
+	node->setParam("/xbot_surface/outliers_searchradius", 0.05); //set high enough that separate clusters won't run into each other
 	node->setParam("/xbot_surface/drive_linearspeed", 0.3); //TODO re-integrate
 	node->setParam("/xbot_surface/drive_angularspeed", 0.4); //TODO re-integrate
+	node->setParam("/xbot_surface/outliers_minneighbors", 2); //will trim out obstacles if set unduly high
 	node->setParam("/xbot_surface/floor_transform", true); //whether to transform and flatten the floor, almost certainly improving the results
+	node->setParam("/xbot_surface/outliers_remove", true); //whether to filter out suspected false positives
 	node->setParam("/xbot_surface/drive_move", false); //set this to actually go somewhere! TODO re-integrate
 	node->setParam("/xbot_surface/display_decisions", false); //limits point cloud output to still frames when the robot decides which way to go TODO re-integrate
 
@@ -163,6 +188,7 @@ int main(int argc, char** argv)
 	:s/set/deletef,dt)f;d$a;j
 	#endif
 	node->deleteParam("/xbot_surface/crop_xradius");
+	node->deleteParam("/xbot_surface/crop_xbumper");
 	node->deleteParam("/xbot_surface/crop_ymin");
 	node->deleteParam("/xbot_surface/crop_ymax");
 	node->deleteParam("/xbot_surface/crop_zmin");
@@ -173,9 +199,12 @@ int main(int argc, char** argv)
 	node->deleteParam("/xbot_surface/floor_farz");
 	node->deleteParam("/xbot_surface/floor_tolerance");
 	node->deleteParam("/xbot_surface/edges_searchradius");
+	node->deleteParam("/xbot_surface/outliers_searchradius");
 	node->deleteParam("/xbot_surface/drive_linearspeed");
 	node->deleteParam("/xbot_surface/drive_angularspeed");
+	node->deleteParam("/xbot_surface/outliers_minneighbors");
 	node->deleteParam("/xbot_surface/floor_transform");
+	node->deleteParam("/xbot_surface/outliers_remove");
 	node->deleteParam("/xbot_surface/drive_move");
 	node->deleteParam("/xbot_surface/display_decisions");
 
